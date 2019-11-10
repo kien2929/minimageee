5f30357c297bab20ed347fab698b5fac810e7f06
kien2929 <kien2929@gmail.com>
2019-11-10 17:59:27 +0700

    res

--- game.js
+++ game.js
@@ -3,7 +3,6 @@ var score = document.getElementById('score');
 var notif = document.getElementById('status');
 var context = canvas.getContext('2d');
 
-
 var color = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabebe', '#469990', '#e6beff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9'];
 var pad = {
     width: 70,
