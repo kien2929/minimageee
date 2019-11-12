ad6f681bdf2e67c20e59db219558e4623a4758cc
kien2929 <kien2929@gmail.com>
2019-11-12 13:37:27 +0700

    double tap

--- game.js
+++ game.js
@@ -202,6 +202,15 @@ function updateScore() {
     score.innerHTML = `${userScore}`;
     document.getElementById('score-small').innerHTML = `${userScore}`;
 }
+////////////////////////////
+var time_stamp = 0; // Or Date.now()
+window.addEventListener("touchstart", function(event) {
+    if (event.timeStamp - time_stamp < 300) { // A tap that occurs less than 300 ms from the last tap will trigger a double tap. This delay may be different between browsers.
+        event.preventDefault();
+        return false;
+    }
+    time_stamp = event.timeStamp;
+});
 
 function draw() {
 
